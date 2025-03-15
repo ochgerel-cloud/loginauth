b7dca8e725c15950e0f1df7a6430eeeec6aa9a54
ochgerel-cloud <ochgerel999@gmail.com>
2025-03-15 13:58:49 +0800

    MERN технологиор login-ийг хийж үзэх гэж байна

--- backend/controllers/db.js
+++
@@ -1,9 +0,0 @@
-import knexConnect from "knex";
-
-const knex = knexConnect({
-  client: "better-sqlite3",
-  connection: { filename: "./data/mydb.sqlite" },
-  useNullAsDefault: true,
-});
-
-export default knex;
